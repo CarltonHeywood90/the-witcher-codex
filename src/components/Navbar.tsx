1f8a628e4be8797c73fb218eb7aa4517a4a47a13
carlton <cjwood1@gmail.com>
2026-06-22 09:12:20 -0600

    hot shower!

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 
   return (
     <nav className="border-b border-zinc-900 bg-zinc-950/70 backdrop-blur-md sticky top-0 z-50 px-4 md:px-12 py-4">
-      <div className="max-w-5xl mx-auto flex justify-between items-center">
+      <div className="max-w-5xl mx-auto flex text-white justify-between items-center">
         <Link href="/" className="font-serif text-lg font-bold text-zinc-100">⚔️ Continent Codex</Link>
 
         {/* Hamburger Button (Visible only on mobile) */}
